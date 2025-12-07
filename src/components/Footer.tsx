99c477cf6eaa70285b1c960e4e45887588251927
Felipe Souza <felipesouzadsgn@gmail.com>
2025-12-07 16:29:36 -0300

    Update code

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center">
-      <p className="text-sm text-gray-400">© All rights reserved 2025 • Felipe Souza Design</p>
-    </footer>
-  );
-}
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="text-center">
-        <h2>Header</h2>
-    </header>
-  );
-}
